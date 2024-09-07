d30ded8956a8d19cbf0399484c02a52d78babab4
Mohammad Ikram <ikramullah201812345@gmail.com>
2024-09-07 13:41:43 +0500

    first comt

--- src/Components/CoFounder.jsx
+++ src/Components/CoFounder.jsx
@@ -15,16 +15,16 @@ const CoFounder = () => {
 
   return (
     <div
-      className="flex px-10 py-2 xl:justify-end justify-center"
+      className="flex xsm:px-10 py-2 xl:justify-end justify-center"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <motion.div
-        className="flex gap-4 items-center p-1 px-2 bg-white rounded-full"
+        className="flex gap-4  items-center p-1 px-2 bg-white rounded-full"
         style={{ display: 'flex', gap: '1rem' }} // Ensure flex properties are applied
       >
         <motion.div
-          className="h-full xl:w-[55px] w-[40px] bg-black rounded-full overflow-hidden"
+          className="h-full xl:w-[55px] w-[50px] xsm:w-[40px] bg-black rounded-full overflow-hidden"
           animate={{ x: isReversed ? 170 : 0 }} // Shift position to simulate reverse
           transition={{ duration: 0.5, ease: 'easeInOut' }}
         >
