2befb54db0f60a2d1931a87103e16ad59ac62acb
Mohammad Ikram <ikramullah201812345@gmail.com>
2024-09-07 14:01:05 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Carousel from './Components/Carousel'
@@ -10,9 +10,23 @@ import Line from './Components/Line'
 import ReviewCarousel from './Components/ReviewCarousel'
 import Footer from './Components/Footer'
 import './font.css';
+import LocomotiveScroll from 'locomotive-scroll';
+import 'locomotive-scroll/dist/locomotive-scroll.css';
 const App = () => {
+  const scrollRef = useRef(null);
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollRef.current,
+      smooth: true,
+      lerp: 0.03, 
+    });
+
+    return () => {
+      scroll.destroy();
+    };
+  }, []);
   return (
-    <div className='font-school '>
+    <div data-scroll-container ref={scrollRef}  className='font-school '>
       <Navbar/>
       <Hero/>
       <Carousel/>
