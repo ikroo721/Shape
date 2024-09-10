a584f7a1bbc6e00f32b882445670e6a69c6661e1
Mohammad Ikram <ikramullah201812345@gmail.com>
2024-09-10 12:57:29 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Minify with esbuild (default, fast and efficient)
+    minify: 'esbuild', // You can switch to 'terser' if you need more advanced options but it will be slower.
+
+    // Uncomment to use Terser if more complex minification is needed:
+    // minify: 'terser',
+    // terserOptions: {
+    //   compress: {
+    //     drop_console: true, // Example option to drop console statements
+    //   },
+    // },
+
+    // Additional settings for fine-tuning
+    cssCodeSplit: true, // Split CSS into separate files (enabled by default)
+    sourcemap: false,   // Disable sourcemaps in production to save space
+    chunkSizeWarningLimit: 500, // Adjust chunk size limit warnings if necessary
+  },
 })
